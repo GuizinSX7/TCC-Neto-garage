18c4cae6c4c94bea506ad2523dd7b76541dc7a5a
Guilherme Dallacqua <53446795804@escola928.edu>
2025-02-12 11:20:59 -0300

    firebase

--- tcc_neto_garage/lib/main.dart
+++ tcc_neto_garage/lib/main.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:tcc_neto_garage/shared/style.dart';
+import 'package:firebase_core/firebase_core.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
