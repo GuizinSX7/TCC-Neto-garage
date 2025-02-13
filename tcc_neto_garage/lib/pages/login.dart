8e81be4e3e683d3605c7a9238627c5c19801646e
Guilherme Dallacqua <53446795804@escola928.edu>
2025-02-13 17:11:21 -0300

    login-processo

--- tcc_neto_garage/lib/pages/login.dart
+++ tcc_neto_garage/lib/pages/login.dart
@@ -9,14 +9,69 @@ class Login extends StatefulWidget {
 }
 
 class _LoginState extends State<Login> {
+
+  final _formKey = GlobalKey<FormState>();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Container(
-          decoration: BoxDecoration(
-            gradient: MyColors.gradienteLoginECadastro,
+      body: Container(
+        decoration: BoxDecoration(
+          gradient: MyColors.gradienteLoginECadastro, 
+        ),
+        child: Center(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              const SizedBox(height: 180,),
+              Image.asset('assets/images/Logo.png'), 
+              const SizedBox(height: 63,),
+              Form(
+                key: _formKey,
+                child: Column(
+                  children: [
+                    SizedBox(
+                      width: 300,
+                      height: 45,
+                      child: TextFormField(
+                        decoration: InputDecoration(
+                          fillColor: const Color.fromARGB(30, 233, 236, 239),
+                          filled: true,
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(20),
+                            borderSide: BorderSide(color: MyColors.branco1, width: 3),
+                          ),
+                          hintText: "Email ou CPF",
+                          hintStyle: TextStyle(color: MyColors.branco4,),
+                          contentPadding: const EdgeInsets.symmetric(vertical:10, horizontal: 20),
+                        ),
+                      ),
+                    ),
+                    const SizedBox(height: 48,),
+                    SizedBox(
+                      width: 300,
+                      height: 45,
+                      child: TextFormField(
+                        decoration: InputDecoration(
+                          fillColor: const Color.fromARGB(30, 233, 236, 239),
+                          filled: true,
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(20),
+                            borderSide: BorderSide(color: MyColors.branco1, width: 3),
+                          ),
+                          hintText: "Senha",
+                          hintStyle: TextStyle(color: MyColors.branco4,),
+                          contentPadding: const EdgeInsets.symmetric(vertical:10, horizontal: 20),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
           ),
         ),
+      ),
     );
   }
 }
