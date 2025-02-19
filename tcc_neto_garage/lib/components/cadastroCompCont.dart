c26fab3bfd65ef2a929f4112fdbeb838074fb8c0
gabrielpires11 <gabri4590@gmail.com>
2025-02-19 15:13:59 -0300

    Configurações finais

--- tcc_neto_garage/lib/components/cadastroCompCont.dart
+++ tcc_neto_garage/lib/components/cadastroCompCont.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
@@ -5,13 +6,12 @@ import 'dart:convert';
 import 'package:tcc_neto_garage/shared/style.dart';
 
 class Continuar extends StatefulWidget {
-
   final VoidCallback voltar;
 
   const Continuar({super.key, required this.voltar});
 
   @override
-  _ContinuarState  createState() => _ContinuarState();
+  _ContinuarState createState() => _ContinuarState();
 }
 
 class _ContinuarState extends State<Continuar> {
@@ -22,6 +22,13 @@ class _ContinuarState extends State<Continuar> {
   final TextEditingController numeroController = TextEditingController();
 
   Future<void> buscarCep(String cep) async {
+    if (cep.length != 8) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text('CEP inválido! Digite um CEP com 8 dígitos.')),
+      );
+      return;
+    }
+
     final url = Uri.parse('https://viacep.com.br/ws/$cep/json/');
 
     try {
@@ -49,25 +56,38 @@ class _ContinuarState extends State<Continuar> {
     }
   }
 
-  Widget buildTextField(
-      {required String hintText, required TextEditingController controller, TextInputType keyboardType = TextInputType.text}) {
+  Widget buildTextField({
+    required String hintText,
+    required TextEditingController controller,
+    TextInputType keyboardType = TextInputType.text,
+    void Function(String)? onFieldSubmitted,
+  }) {
     return SizedBox(
       width: 300,
       height: 45,
       child: TextFormField(
         controller: controller,
+        cursorColor: MyColors.branco1,
         keyboardType: keyboardType,
-        textAlign: TextAlign.center, // Centraliza o placeholder
         decoration: InputDecoration(
-          hintText: hintText,
-          labelStyle: TextStyle(color: MyColors.branco4),
+          fillColor: const Color.fromARGB(30, 233, 236, 239),
           filled: true,
-          fillColor: MyColors.branco3.withOpacity(0.3),
           border: OutlineInputBorder(
             borderRadius: BorderRadius.circular(20),
-            borderSide: BorderSide(color: MyColors.branco1),
+            borderSide: BorderSide(color: MyColors.branco1, width: 3),
           ),
+          focusedBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(20),
+            borderSide: BorderSide(color: MyColors.branco1, width: 1),
+          ),
+          hintText: hintText,
+          hintStyle: TextStyle(
+            color: MyColors.branco4,
+          ),
+          contentPadding:
+              const EdgeInsets.symmetric(vertical: 10, horizontal: 20),
         ),
+        onFieldSubmitted: onFieldSubmitted,
       ),
     );
   }
@@ -77,7 +97,7 @@ class _ContinuarState extends State<Continuar> {
     return Center(
       child: Form(
         child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center, // Centraliza os inputs
+          crossAxisAlignment: CrossAxisAlignment.center,
           children: [
             const SizedBox(height: 40),
             Text(
@@ -89,23 +109,36 @@ class _ContinuarState extends State<Continuar> {
                   color: MyColors.branco1),
             ),
             const SizedBox(height: 39),
-
-            buildTextField(hintText: "CEP", controller: cepController, keyboardType: TextInputType.number),
+            buildTextField(
+              hintText: "CEP",
+              controller: cepController,
+              keyboardType: TextInputType.number,
+              onFieldSubmitted: (value) => buscarCep(value),
+            ),
             SizedBox(height: 30),
             buildTextField(hintText: "Bairro", controller: bairroController),
             SizedBox(height: 30),
-            buildTextField(hintText: "Logradouro", controller: logradouroController),
+            buildTextField(
+                hintText: "Logradouro", controller: logradouroController),
             SizedBox(height: 30),
-            buildTextField(hintText: "Número", controller: numeroController, keyboardType: TextInputType.number),
+            buildTextField(
+                hintText: "Número",
+                controller: numeroController,
+                keyboardType: TextInputType.number),
             SizedBox(height: 30),
             buildTextField(hintText: "Cidade", controller: cidadeController),
             SizedBox(height: 50),
-
-            // Botão Cadastrar
             Center(
               child: ElevatedButton(
                 onPressed: () {},
-                child: Text("Cadastrar"),
+                child: Text(
+                  "Cadastrar",
+                  style: TextStyle(
+                    fontSize: 14, // Tamanho maior para melhor legibilidade
+                    fontWeight: FontWeight.bold, // Negrito para destaque
+                    fontFamily: MyFonts.fontTerc, // Mantendo a identidade visual
+                  ),
+                ),
                 style: ElevatedButton.styleFrom(
                   fixedSize: Size(300, 49),
                   backgroundColor: MyColors.azul2,
@@ -117,24 +150,30 @@ class _ContinuarState extends State<Continuar> {
               ),
             ),
             SizedBox(height: 30),
-
-            GestureDetector(
-              onTap: () {
-                widget.voltar();
-              },
-              child: Text(
-                "Aqui",
+            Container(
+              width: 245,
+              height: 1,
+              decoration: BoxDecoration(color: MyColors.branco1),
+            ),
+            SizedBox(height: 13),
+            RichText(
+              text: TextSpan(
                 style: TextStyle(
                   fontFamily: MyFonts.fontTerc,
                   fontSize: 14,
-                  color: MyColors.azul1, // Adiciona cor para parecer um link
+                  color: MyColors.branco1,
                 ),
+                children: [
+                  TextSpan(text: "Voltar ao Cadastro? "),
+                  TextSpan(
+                    text: "Aqui",
+                    style: TextStyle(
+                      color: MyColors.azul1,
+                    ),
+                    recognizer: TapGestureRecognizer()..onTap = widget.voltar,
+                  ),
+                ],
               ),
-            ),
-
-            Text(
-              "Voltar ao Cadastro?",
-              style: TextStyle(fontFamily: MyFonts.fontTerc, fontSize: 14, color: MyColors.branco1),
             ),
           ],
         ),
