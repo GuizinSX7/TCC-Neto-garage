246c0dc5fba6cfb04ff44fb7938789854867ec43
gabrielpires16 <gabri4590@gmail.com>
2025-03-14 10:46:38 -0300

    cores e bordas

--- tcc_neto_garage/lib/components/cadastroveiculoComp.dart
+++ tcc_neto_garage/lib/components/cadastroveiculoComp.dart
@@ -24,7 +24,8 @@ class _CadastroveiculoState extends State<Cadastroveiculo> {
     return Scaffold(
       backgroundColor: Colors.black,
       body: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 20), // Adiciona espaçamento lateral
+        padding: const EdgeInsets.symmetric(
+            horizontal: 20), // Adiciona espaçamento lateral
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.center,
           children: [
@@ -58,13 +59,17 @@ class _CadastroveiculoState extends State<Cadastroveiculo> {
                   children: [
                     // Ícones superiores (Picape, Moto, Hatch)
                     Row(
-                      mainAxisAlignment: MainAxisAlignment.center, // Centraliza os ícones na linha
+                      mainAxisAlignment: MainAxisAlignment
+                          .center, // Centraliza os ícones na linha
                       children: [
-                        _buildSelectableIcon("picape", "assets/icons/Picape.png", "Picape"),
+                        _buildSelectableIcon(
+                            "picape", "assets/icons/Picape.png", "Picape"),
                         const SizedBox(width: 39.11),
-                        _buildSelectableIcon("moto", "assets/icons/Moto.png", "Moto"),
+                        _buildSelectableIcon(
+                            "moto", "assets/icons/Moto.png", "Moto"),
                         const SizedBox(width: 39.11),
-                        _buildSelectableIcon("hatch", "assets/icons/Hatch.png", "Hatch"),
+                        _buildSelectableIcon(
+                            "hatch", "assets/icons/Hatch.png", "Hatch"),
                       ],
                     ),
                     const SizedBox(height: 32.11),
@@ -72,9 +77,11 @@ class _CadastroveiculoState extends State<Cadastroveiculo> {
                     Row(
                       mainAxisAlignment: MainAxisAlignment.center,
                       children: [
-                        _buildSelectableIcon("suv", "assets/icons/SUV.png", "SUV"),
+                        _buildSelectableIcon(
+                            "suv", "assets/icons/SUV.png", "SUV"),
                         const SizedBox(width: 39.11),
-                        _buildSelectableIcon("sedan", "assets/icons/Sedan.png", "Sedan"),
+                        _buildSelectableIcon(
+                            "sedan", "assets/icons/Sedan.png", "Sedan"),
                       ],
                     ),
                   ],
@@ -86,19 +93,41 @@ class _CadastroveiculoState extends State<Cadastroveiculo> {
             buildForm(context), // Adiciona o formulário abaixo dos ícones
 
             // Botões de cores
-            const SizedBox(height: 48), // Espaço entre o campo "Placa:" e os botões de cor
+            const SizedBox(
+                height: 48), // Espaço entre o campo "Placa:" e os botões de cor
+            // Container para os botões de cores
+            // Container para os botões de cores
+            // Container para os botões de cores
             Container(
               width: 304,
               height: 166,
               decoration: BoxDecoration(
                 color: const Color.fromARGB(75, 233, 236, 239),
                 borderRadius: BorderRadius.circular(20),
+                border: Border.all(
+                    color: MyColors.branco1, width: 3), // Borda adicionada aqui
               ),
+              padding: const EdgeInsets.symmetric(
+                  horizontal: 16, vertical: 12), // Adiciona padding interno
               child: Column(
+                crossAxisAlignment:
+                    CrossAxisAlignment.start, // Alinha o texto à esquerda
                 children: [
+                  // HintText "Cor:"
+                  const Text(
+                    "Cor:",
+                    style: TextStyle(
+                      fontSize: 14,
+                      fontFamily: MyFonts.fontTerc,
+                      color: MyColors.branco1, // Ajuste conforme necessário
+                    ),
+                  ),
+                  const SizedBox(
+                      height: 16), // Espaçamento de 16px antes dos botões
+
                   // Primeira linha de botões de cores
                   Row(
-                    mainAxisAlignment: MainAxisAlignment.center, // Centraliza os botões de cores
+                    mainAxisAlignment: MainAxisAlignment.center,
                     children: [
                       _buildColorButton("color1", Color(0xFF14532D)),
                       const SizedBox(width: 13.05),
@@ -113,7 +142,9 @@ class _CadastroveiculoState extends State<Cadastroveiculo> {
                       _buildColorButton("color6", MyColors.branco1),
                     ],
                   ),
-                  const SizedBox(height: 32.11),
+                  const SizedBox(
+                      height: 16), // Mantém espaçamento igual entre as linhas
+
                   // Segunda linha de botões de cores
                   Row(
                     mainAxisAlignment: MainAxisAlignment.center,
@@ -141,7 +172,8 @@ class _CadastroveiculoState extends State<Cadastroveiculo> {
   }
 
   // Widget para construir os ícones clicáveis com texto
-  Widget _buildSelectableIcon(String iconName, String assetPath, String vehicleName) {
+  Widget _buildSelectableIcon(
+      String iconName, String assetPath, String vehicleName) {
     bool isSelected = selectedIcon == iconName;
 
     return Column(
@@ -177,7 +209,7 @@ class _CadastroveiculoState extends State<Cadastroveiculo> {
     );
   }
 
-   // Função para criar os botões de cor
+  // Função para criar os botões de cor
   Widget _buildColorButton(String colorName, Color color) {
     bool isSelected = selectedIcon == colorName;
 
@@ -200,7 +232,8 @@ class _CadastroveiculoState extends State<Cadastroveiculo> {
                   width: 18, // Tamanho do círculo
                   height: 18,
                   decoration: BoxDecoration(
-                    color: color == MyColors.branco1 ? Colors.black : Colors.white,
+                    color:
+                        color == MyColors.branco1 ? Colors.black : Colors.white,
                     shape: BoxShape.circle,
                   ),
                 ),
@@ -209,69 +242,66 @@ class _CadastroveiculoState extends State<Cadastroveiculo> {
       ),
     );
   }
-Widget buildForm(BuildContext context) {
+
+  Widget buildForm(BuildContext context) {
     return Center(
       child: Form(
         key: _formKey,
-        child: Column(
-          children: [
-            SizedBox(
-              width: 304,
-              height: 45,
-              child: Material(
-                color: Colors.transparent,
-                child: TextFormField(
-                  cursorColor: MyColors.branco1,
-                  decoration: InputDecoration(
-                    fillColor: const Color.fromARGB(30, 233, 236, 239),
-                    filled: true,
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(20),
-                      borderSide: BorderSide(color: MyColors.branco1, width: 3),
-                    ),
-                    focusedBorder: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(20),
-                      borderSide: BorderSide(color: MyColors.branco1, width: 1),
-                    ),
-                    hintText: "Marca: ",
-                    hintStyle: TextStyle(
-                      color: MyColors.branco4,
-                    ),
+        child: Column(children: [
+          Container(
+            width: 304,
+            height: 45,
+            decoration: BoxDecoration(
+              border: Border.all(color: MyColors.branco1, width: 3),
+              borderRadius: BorderRadius.circular(20),
+            ),
+            child: Material(
+              color: Colors.transparent,
+              child: TextFormField(
+                cursorColor: MyColors.branco1,
+                decoration: InputDecoration(
+                  fillColor: const Color.fromARGB(75, 233, 236, 239),
+                  filled: true,
+                  hintText: "Marca: ",
+                  hintStyle: TextStyle(
+                    color: MyColors.branco1,
+                    fontSize: 14,
+                    fontFamily: MyFonts.fontTerc,
                   ),
+                  border: InputBorder.none,
                 ),
               ),
             ),
-            const SizedBox(height: 48),
-            SizedBox(
-              width: 304,
-              height: 45,
-              child: Material(
-                color: Colors.transparent,
-                child: TextFormField(
-                  cursorColor: MyColors.branco1,
-                  decoration: InputDecoration(
-                    fillColor: const Color.fromARGB(30, 233, 236, 239),
-                    filled: true,
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(20),
-                      borderSide: BorderSide(color: MyColors.branco1, width: 3),
-                    ),
-                    focusedBorder: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(20),
-                      borderSide: BorderSide(color: MyColors.branco1, width: 1),
-                    ),
-                    hintText: "Placa: ",
-                    hintStyle: TextStyle(
-                      color: MyColors.branco4,
-                    ),
+          ),
+          const SizedBox(height: 48),
+          Container(
+            width: 304,
+            height: 45,
+            decoration: BoxDecoration(
+              border: Border.all(color: MyColors.branco1, width: 3),
+              borderRadius: BorderRadius.circular(20),
+            ),
+            
+            child: Material(
+              color: Colors.transparent,
+              child: TextFormField(
+                cursorColor: MyColors.branco1,
+                decoration: InputDecoration(
+                  fillColor: const Color.fromARGB(75, 233, 236, 239),
+                  filled: true,
+                  hintText: "Placa: ",
+                  hintStyle: TextStyle(
+                    color: MyColors.branco1,
+                    fontSize: 14,
+                    fontFamily: MyFonts.fontTerc,
                   ),
+                  border: InputBorder.none,
                 ),
               ),
             ),
-          ],
-        ),
+          ),
+        ]),
       ),
     );
   }
 }
-
