7133446a340a05d2d7c074b1f385b79bb39fae48
gabrielpires11 <gabri4590@gmail.com>
2025-04-03 17:14:43 -0300

    Splash_modificações

--- tcc_neto_garage/lib/components/splash_screen.dart
+++ tcc_neto_garage/lib/components/splash_screen.dart
@@ -1,6 +1,6 @@
-import 'package:animated_splash_screen/animated_splash_screen.dart';
 import 'package:flutter/material.dart';
-import 'package:tcc_neto_garage/main.dart';
+import 'package:lottie/lottie.dart';
+import 'package:animate_do/animate_do.dart';
 
 void main() {
   runApp(const MyApp());
@@ -13,43 +13,121 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: AnimatedSplashScreen(
-        splash: 'assets/img/GifNetoGarage.gif',
-        splashIconSize: 500.0, // Ajustado para um tamanho mais razoável
-        nextScreen: const Home(),
-        backgroundColor: Colors.black,
-        duration: 3100,
-      ),
+      home: const SplashScreen(),
     );
   }
 }
 
-class Home extends StatelessWidget {
-  const Home({super.key});
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  bool showFirstAnimation = true;
+
+  @override
+  void initState() {
+    super.initState();
+    Future.delayed(const Duration(seconds: 3), () {
+      setState(() {
+        showFirstAnimation = false;
+      });
+    });
+    Future.delayed(const Duration(seconds: 6), () {
+      Navigator.of(context).pushReplacement(
+        MaterialPageRoute(builder: (context) => const HomeScreen()),
+      );
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.black,
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Image.asset(
-              'assets/img/LogoN.png',
-              width: 80.0,
-            ),
-            const SizedBox(height: 20), // Adicionando um espaçamento
-            const Text(
-              'Carregando...',
-              textAlign: TextAlign.center,
-              style: TextStyle(
-                fontSize: 30.0, // Reduzi para melhor adaptação
-                fontWeight: FontWeight.bold,
-                color: Colors.white,
-              ),
-            ),
-          ],
+        child: AnimatedSwitcher(
+          duration: const Duration(seconds: 1),
+          child: showFirstAnimation
+              ? Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    TweenAnimationBuilder(
+                      tween: Tween<double>(begin: 0.5, end: 5.0),
+                      duration: const Duration(seconds: 3),
+                      builder: (context, double scale, child) {
+                        return Transform.scale(
+                          scale: scale,
+                          child: child,
+                        );
+                      },
+                      onEnd: () {
+                        Future.delayed(const Duration(seconds: 1), () {
+                          setState(() {
+                            showFirstAnimation = false;
+                          });
+                        });
+                      },
+                      child: Image.asset(
+                        'assets/img/LogoN-1.png',
+                        width: 150.0,
+                      ),
+                    ),
+                    const SizedBox(height: 20),
+                    // const Text(
+                    //   'Carregando...',
+                    //   style: TextStyle(
+                    //     fontSize: 28.0,
+                    //     fontWeight: FontWeight.bold,
+                    //     color: Colors.white,
+                    //   ),
+                    // ),
+                  ],
+                )
+              : Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Pulse(
+                      infinite: true,
+                      child: Image.asset(
+                        'assets/img/Logo.png',
+                        width: 200.0,
+                      ),
+                    ),
+                    const SizedBox(height: 20),
+                    // const Text(
+                    //   'Seu carro no brilho máximo!',
+                    //   style: TextStyle(
+                    //     fontSize: 28.0,
+                    //     fontWeight: FontWeight.bold,
+                    //     color: Colors.white,
+                    //   ),
+                    // ),
+                  ],
+                ),
+        ),
+      ),
+    );
+  }
+}
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      body: const Center(
+        child: Text(
+          'Bem-vindo ao Neto Garage!',
+          style: TextStyle(
+            fontSize: 24.0,
+            fontWeight: FontWeight.bold,
+            color: Colors.white,
+          ),
         ),
       ),
     );
